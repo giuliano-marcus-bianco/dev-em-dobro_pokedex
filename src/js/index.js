20f245dd2e781c8011f9110620cb26d01be8d86c
Giuliano Marcus Bianco <96282331+giuliano-marcus-bianco@users.noreply.github.com>
2023-05-14 19:33:12 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -8,8 +8,8 @@ buttonDarkMode.addEventListener("click", () => {
     body.classList.toggle("dark-mode");
 
     if (body.classList.contains("dark-mode")) {
-        imgButtonDarkMode.setAttribute("src", "./src/imgs/sun.png");
-    } else {
         imgButtonDarkMode.setAttribute("src", "./src/imgs/moon.png");
+    } else {
+        imgButtonDarkMode.setAttribute("src", "./src/imgs/sun.png");
     }
 });
